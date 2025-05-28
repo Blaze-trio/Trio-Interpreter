1ce63a7502ff84077ed7a83786415422fbdd8bb1
Blaze-trio <64141242+Blaze-trio@users.noreply.github.com>
2025-05-28 19:47:25 +0300

    made the AST

--- ast.ts
+++ ast.ts
@@ -1,1 +1,22 @@
-export type NodeType = "Number" | "Identifier" | "BinaryOperator" | "Let" | "If" | "Else" | "While" | "For" | "Function";
+export type NodeType = "Program" | "NumericLiteral" | "Identifier" | "BinaryExpr";
+export interface Stemt{
+    kind: NodeType;
+}
+export interface Program extends Stemt {
+    kind: "Program";
+    body: Stemt[];
+}
+export interface Expr extends Stemt {}
+export interface BinaryExpr extends Expr {
+    left : Expr;
+    right: Expr;
+    operator: string;
+}
+export interface Identifier extends Expr {
+    kind: "Identifier";
+    symbol: string;
+}
+export interface NumericLiteral extends Expr {
+    kind: "NumericLiteral";
+    value: number;
+}
